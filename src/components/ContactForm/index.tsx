c3164d51f8f27caed0733aae5205eebf445763f4
MarcialFantini <marcialfantini@gmail.com>
2023-10-22 15:34:32 -0300

    contacto animations update

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "./style.module.css";
 import ContactCard from "../ContactCard";
 const initialFormData = {
@@ -11,11 +11,11 @@ const initialFormData = {
 
 const FormComponent = () => {
   const [formData, setFormData] = useState(initialFormData);
+  const [isSend, setIsSend] = useState(false);
+  const [error, setError] = useState("");
 
   const handleInputChange = (
-    e:
-      | React.ChangeEvent<HTMLInputElement>
-      | React.ChangeEvent<HTMLTextAreaElement>
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
     const { name, value } = e.target;
     setFormData((prevData) => ({
@@ -24,24 +24,81 @@ const FormComponent = () => {
     }));
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // Aquí puedes realizar cualquier acción necesaria con los datos del formulario, como enviarlos a un servidor
-    console.log(formData);
+    try {
+      const nameRegex = /^[a-zA-Z\s]{2,50}$/;
+      const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+      const phoneRegex = /^[\d()+-]{7,15}$/;
+      const textMaxLength = 500;
 
-    fetch("/api", {
-      method: "POST",
-      cache: "no-cache",
-      body: JSON.stringify(formData),
-    });
+      if (!nameRegex.test(formData.name)) {
+        throw new Error("El nombre no cumple con las restricciones.");
+      }
+
+      if (!emailRegex.test(formData.email)) {
+        throw new Error(
+          "El correo electrónico no cumple con las restricciones."
+        );
+      }
+
+      if (!phoneRegex.test(formData.phone)) {
+        throw new Error("El teléfono no cumple con las restricciones.");
+      }
+
+      if (formData.text.length > textMaxLength) {
+        throw new Error("El texto excede el límite de longitud permitido.");
+      }
+
+      const response = await fetch("/api", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        setIsSend(true);
+        setFormData(initialFormData);
+      } else {
+        throw new Error("Error al enviar los datos.");
+      }
+    } catch (error: any) {
+      setError(error.message);
+    }
   };
+
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        setError("");
+      }, 6000);
+    }
+  }, [error]);
+
+  useEffect(() => {
+    if (isSend) {
+      setTimeout(() => {
+        setIsSend(false);
+      }, 6000);
+    }
+  }, [isSend]);
 
   return (
     <form onSubmit={handleSubmit} className={style.form}>
+      <div className={style.messageGood + " " + (isSend ? style.active : "")}>
+        <h2>Enviado Correctamente</h2>
+      </div>
+      <div className={style.messageBad + " " + (error ? style.active : "")}>
+        <h2>Error al enviar</h2>
+        <p>{error}</p>
+      </div>
       <div>
         <label>
           Nombre:
           <input
+            required
             type="text"
             name="name"
             value={formData.name}
@@ -53,6 +110,7 @@ const FormComponent = () => {
         <label>
           Correo:
           <input
+            required
             type="text"
             name="email"
             value={formData.email}
@@ -64,6 +122,7 @@ const FormComponent = () => {
         <label>
           Teléfono :
           <input
+            required
             type="text"
             name="phone"
             value={formData.phone}
@@ -75,6 +134,7 @@ const FormComponent = () => {
         <label>
           Mensaje:
           <textarea
+            required
             className={style.textArea}
             name="text"
             value={formData.text}
